88031ec50ccccb81cc6fd8cea13fad74093910cd
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2016-08-17 13:49:24 +0200

    More features
    Added some more features: Count of Conjunctions, Disjunctions, and added functionality for Comparison and Arithmetic Operators

--- src/neurob/core/features/FeatureCollector.java
+++ src/neurob/core/features/FeatureCollector.java
@@ -29,11 +29,20 @@ public class FeatureCollector extends DepthFirstAdapter {
 		super.caseStart(node);
 	}
 	
-	/*
+//	@Override
+//	public void defaultIn(final Node node){
+//		System.out.println(node.getClass());
+//	}
+	
+	/**************************************************************************
 	 * Following: the methods to extract the feature values from AST
+	 * - Quantifiers
+	 * - Arithmetic Operators
+	 * - Comparison Operators
+	 * - Logical Operators
 	 */
 	
-	// Predicates
+	// Quantifiers
 	@Override
 	public void caseAForallPredicate(final AForallPredicate node) {
 		fd.incForAllQuantifiersCount();
@@ -45,5 +54,72 @@ public class FeatureCollector extends DepthFirstAdapter {
 		fd.incExistsQuantifiersCount();
 		node.getPredicate().apply(this);
 	}
+	
+	// Arithmetic Operators
+	@Override
+	public void caseAAddExpression(final AAddExpression node){
+		fd.incArithmOperatorsCount();
+		node.getLeft().apply(this);
+		node.getRight().apply(this);
+	}
+	@Override
+	public void caseAMinusOrSetSubtractExpression(final AMinusOrSetSubtractExpression node){
+		fd.incArithmOperatorsCount();
+		node.getLeft().apply(this);
+		node.getRight().apply(this);
+	}
+	@Override
+	public void caseAMultOrCartExpression(final AMultOrCartExpression node){
+		fd.incArithmOperatorsCount();
+		node.getLeft().apply(this);
+		node.getRight().apply(this);
+	}
+	@Override
+	public void caseADivExpression(final ADivExpression node){
+		fd.incArithmOperatorsCount();
+		node.getLeft().apply(this);
+		node.getRight().apply(this);
+	}
+	
+	// comparisons
+	@Override
+	public void caseAEqualPredicate(final AEqualPredicate node){
+		fd.incCompOperatorsCount();
+		node.getLeft().apply(this);
+		node.getRight().apply(this);
+	}
+	@Override
+	public void caseANotEqualPredicate(final ANotEqualPredicate node){
+		fd.incCompOperatorsCount();
+		node.getLeft().apply(this);
+		node.getRight().apply(this);
+	}
+	@Override
+	public void caseAGreaterPredicate(final AGreaterPredicate node){
+		fd.incCompOperatorsCount();
+		node.getLeft().apply(this);
+		node.getRight().apply(this);
+	}
+	@Override
+	public void caseALessPredicate(final ALessPredicate node){
+		fd.incCompOperatorsCount();
+		node.getLeft().apply(this);
+		node.getRight().apply(this);
+	}
+	
+	// logical operators
+	@Override
+	public void caseAConjunctPredicate(final AConjunctPredicate node){
+		fd.incConjunctionsCount();
+		node.getLeft().apply(this);
+		node.getRight().apply(this);
+	}
+	@Override
+	public void caseADisjunctPredicate(final ADisjunctPredicate node){
+		fd.incDisjunctionsCount();
+		node.getLeft().apply(this);
+		node.getRight().apply(this);
+	}
+	
 
 }
--- src/neurob/core/features/FeatureData.java
+++ src/neurob/core/features/FeatureData.java
@@ -17,21 +17,32 @@ public class FeatureData {
 	private int fForAllQuantifiersCount; // number of universal quantifiers
 	private int fArithmOperatorsCount; // number of arithmetic operators
 	private int fCompOperatorsCount; // number of comparison operators
+	private int fConjunctionsCount; // number of conjunctions
+	private int fDisjunctionsCount; // number of disjunctions
 
 	public FeatureData() {
 		// set initial values
 		fFormulaLength = 0;
+		// Quantifiers
 		fExistsQuantifiersCount = 0;
-		fForAllQuantifiersCount = 0; 
+		fForAllQuantifiersCount = 0;
+		// Operators
 		fArithmOperatorsCount = 0;
 		fCompOperatorsCount = 0;
+		fConjunctionsCount = 0;
+		fDisjunctionsCount = 0;
+		
 	}
 	
 	@Override
 	public String toString(){
 		String s = "Formula Length: ? (NYI)\n"
+				+ "Arithmetic Operators: "+fArithmOperatorsCount+ "\n"
+				+ "Comparison Operators: "+fCompOperatorsCount+ "\n"
 				+ "Universal Quantifiers: "+fForAllQuantifiersCount+ "\n"
-				+ "Existential Quantifiers: "+fExistsQuantifiersCount+"\n";
+				+ "Existential Quantifiers: "+fExistsQuantifiersCount+"\n"
+				+ "Conjunctions: "+fConjunctionsCount+"\n"
+				+ "Disjunctions: "+fDisjunctionsCount+"\n";
 		return s;
 	}
 	
@@ -44,5 +55,17 @@ public class FeatureData {
 	
 	public int getForAllQuantifiersCount(){ return fForAllQuantifiersCount; }
 	public void incForAllQuantifiersCount(){ fForAllQuantifiersCount++; }
+	
+	public int getArithmOperatorsCount(){ return fArithmOperatorsCount; }
+	public void incArithmOperatorsCount(){ fArithmOperatorsCount++; }
+	
+	public int getCompOperatorsCount(){ return fCompOperatorsCount; }
+	public void incCompOperatorsCount(){ fCompOperatorsCount++; }
+	
+	public int getConjunctionsCount(){ return fConjunctionsCount; }
+	public void incConjunctionsCount(){ fConjunctionsCount++; }
+	
+	public int getDisjunctionsCount(){ return fDisjunctionsCount; }
+	public void incDisjunctionsCount(){ fDisjunctionsCount++; }
 
 }
