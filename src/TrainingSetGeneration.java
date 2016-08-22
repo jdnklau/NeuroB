dcdf4d871259cb48767a39d56a5548d50d6491c4
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2016-08-22 15:07:34 +0200

    Added output to training set generation.
    Changed used outpur directory.

--- src/TrainingSetGeneration.java
+++ src/TrainingSetGeneration.java
@@ -8,12 +8,16 @@ public class TrainingSetGeneration {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Beginning to generate training set...");
+		
 		TrainingSetGenerator tsg = new TrainingSetGenerator();
 		
 		Path sourceDir = Paths.get("prob_examples/public_examples/B/");
-		Path targetDir = Paths.get("prob_examples/training_data/B/");
+		Path targetDir = Paths.get("prob_examples/training_data/public_examples/B/");
 		
 		tsg.generateTrainingSet(sourceDir, targetDir);
+		
+		System.out.println("... finished generation of training set.");
 	}
 
 }
