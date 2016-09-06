838fbdab476099169662b74c5e7e00ca413efa9d
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2016-09-06 18:17:35 +0200

    Added utility to generate list of subdirectories.
    This is to increase the speed of detection of problematic files in the prob_examples. The TrainingSetGeneration for now utilises this list (has to be created with GenerateDirectoryHierarchyList).

--- src/TrainingSetGeneration.java
+++ src/TrainingSetGeneration.java
@@ -1,6 +1,9 @@
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 import neurob.training.TrainingSetGenerator;
 import neurob.training.generators.DefaultTrainingDataCollector;
@@ -13,10 +16,24 @@ public class TrainingSetGeneration {
 		
 		TrainingSetGenerator tsg = new TrainingSetGenerator(new DefaultTrainingDataCollector());
 		
-		Path sourceDir = Paths.get("prob_examples/public_examples/B/");
-		Path targetDir = Paths.get("prob_examples/training_data/public_examples/B/");
+//		Path sourceDir = Paths.get("prob_examples/reduced_examples/B/Tickets");
+//		Path targetDir = Paths.get("prob_examples/training_data/public_examples/B/Tickets");
+//		
+//		tsg.generateTrainingSet(sourceDir, targetDir);
 		
-		tsg.generateTrainingSet(sourceDir, targetDir);
+		try {
+			for( String line : Files.readAllLines(Paths.get("prob_examples/directories.txt"))){
+					Path src = Paths.get("prob_examples/reduced_examples/"+line);
+					Path tar = Paths.get("prob_examples/training_data/public_examples");
+					
+					tsg.generateTrainingSet(src, tar, false);
+					
+					// TODO: delete line from file
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 //		Path sourceDir = Paths.get("prob_examples/public_examples/B/Tickets/Hansen23_WhilePerformance/WhileSlow_CartProduct.mch");
 //		Path targetDir = Paths.get("prob_examples/training_data/public_examples/B/Tickets/Hansen23_WhilePerformance/WhileSlow_CartProduct.nbtrain");
