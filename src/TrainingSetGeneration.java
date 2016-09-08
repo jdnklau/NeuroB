362506d7f158a2a24bb0a2828c5df5d4b64e02a9
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2016-09-08 11:45:27 +0200

    Using public_examples instead of reduced_examples again

--- src/TrainingSetGeneration.java
+++ src/TrainingSetGeneration.java
@@ -16,7 +16,7 @@ public class TrainingSetGeneration {
 		
 		TrainingSetGenerator tsg = new TrainingSetGenerator(new DefaultTrainingDataCollector());
 		
-		Path sourceDir = Paths.get("prob_examples/reduced_examples/B/");
+		Path sourceDir = Paths.get("prob_examples/public_examples/B/");
 		Path targetDir = Paths.get("prob_examples/training_data/public_examples/B/");
 		
 		tsg.generateTrainingSet(sourceDir, targetDir);
