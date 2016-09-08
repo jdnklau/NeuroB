e952f3b9897acec38293fe4a881cd036b720c744
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2016-09-08 11:11:25 +0200

    Added analysis tool for training data.

---
+++ src/AnalyseTrainingSet.java
@@ -0,0 +1,14 @@
+import java.nio.file.Paths;
+
+import neurob.training.TrainingSetAnalyser;
+
+public class AnalyseTrainingSet {
+
+	public static void main(String[] args) {
+		TrainingSetAnalyser tsa = new TrainingSetAnalyser();
+		tsa.analyseTrainingSet(Paths.get("prob_examples/training_data"));
+		tsa.getStatistics();
+
+	}
+
+}
