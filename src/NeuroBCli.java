f239d231b3803754449810a067f92b5372477762
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2016-09-19 13:51:43 +0200

    Made code of cli more elegant.

--- src/NeuroBCli.java
+++ src/NeuroBCli.java
@@ -3,6 +3,8 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import neurob.NeuroB;
 import neurob.core.nets.DefaultPredicateSolverPredictionNet;
@@ -14,67 +16,133 @@ import neurob.training.generators.DefaultTrainingDataCollector;
 public class NeuroBCli {
 	private static final Path libraryIOpath = Paths.get("prob_examples/LibraryIO.def");
 	private static NeuroB nb;
+	private static Path dir;
+	private static Path tar;
+	private static HashMap<String, ArrayList<String>> ops;
 
 	public static void main(String[] args) {
-		// set up net to use
-		nb = new NeuroB(
-				new DefaultPredicateSolverPredictionNet()
-				.setSeed(0L)
-				.build()
-			);
+		// set up options hash map
+		ops = new HashMap<String, ArrayList<String>>();		
 		
-		// default start
-		if(args.length < 1){
-			NeuroBCli.trainingSetGeneration();
-		}
-		
-		// check command
-		else if(args[0].equals("help")){
-			// TODO: write a help text
-			System.out.println("TODO: write a help text");
-		}
-		
-		// Distribute the LibraryIO.def
-		else if(args[0].equals("libraryIODef")) {
-			if(args.length < 2) {
-				System.out.println("libraryIODef needs a second argument, the directory path into which the file shall be distributed");
-			}
-			else {
-				NeuroBCli.distribute(Paths.get(args[1]));
-			}
-			
-		}
-		
-		// generate or analyse training set
-		else if(args[0].equals("trainingset")) {
-			if (args.length > 2) {
-				if(args[1].equals("-analyse")){
-					NeuroBCli.analyseTrainingSet(Paths.get(args[2]));
+		// collect command line options
+		if(args.length >= 2){
+			ArrayList<String> values = null;
+			for(int i = 1; i<args.length; ++i){
+				if(args[i].charAt(0) == '-'){
+					// get option name
+					String opt = args[i].substring(1);
+					values = new ArrayList<String>();
+					ops.put(opt, values);
 				}
-				// generate csv from nbtrain files
-				else if(args[1].equals("-csv")){
-					NeuroBCli.trainingCSVGeneration(Paths.get(args[2]));
+				else if(values != null){
+					values.add(args[i]);
 				}
-			}
-			else {
-				NeuroBCli.trainingSetGeneration();
-			}
-		}
-		
-		// generate a single nbtrain file
-		else if(args[0].equals("trainingfile")) {
-			if(args.length < 2) {
-				System.out.println("trainingfile needs a second argument, the path to the file in question");
-			}
-			else {
-				NeuroBCli.singleTrainingDataGeneration(Paths.get(args[1]));
+				else {
+					System.out.println("Unknown argument "+args[i]);
+					return;
+				}
+				
 			}
 		}
 		
+		NeuroBCli.parseCommandLineOptions(ops);
 		
+		// extract command
+		String cmd;
+		if(args.length < 1){ // no commands
+			cmd = "trainingset"; // default
+		}
+		else {
+			cmd = args[0];
+		}
+		
+		// execute command
+		
+		if(cmd.equals("help")){
+			String help =
+					  "Call with the following arguments, where -net <net> indicates the neural net to be used (see below):\n"
+					
+					+ "trainingset [-dir <directory>] [-net <net>]\n"
+					+ "\tGenerate training data from the mch files found in <directory>\n"
+					
+					+ "trainingset [-dir <directory>] -file <file> [-net <net>]\n"
+					+ "\tGenerate training data from a specific file. <file> has to be given relative to <directory>, which contains <file>\n"
+					
+					+ "trainingset -analyse [-tar <directory>] [-net <net>]\n"
+					+ "\tAnalyse the generated training data in <directory>\n"
 
+					+ "trainingset -csv [-tar <directory>]\n"
+					+ "\tGenerate csv file from nbtrain files in <directory>\n"
+					
+					+ "libraryIODef -tar <directory>\n"
+					+ "\tDistributes the LibraryIO.def file in <directory>\n"
+					
+					+ "\nNotes:\n"
+					+ "- if -dir <directory> is not set, it defaults to prob_examples/public_examples/B/\n"
+					+ "- if -tar <directory> is not set, it defaults to training_data/manual_call/\n"
+					+ "- if -net <net> is not set, it defaults to 'default' net\n"
+					
+					+ "\nNets:\n"
+					+ "The implemented nets you can access via the cli are\n"
+					+ "\tdefault - The default implementation"
+					;
+							  
+		}
+		// Generate training data
+		else if(cmd.equals("trainingset")){
+			// analyse training set
+			if(ops.containsKey("analyse")){
+				analyseTrainingSet(tar);
+			}
+			// generate csv
+			else if(ops.containsKey("csv")){
+				trainingCSVGeneration(tar);
+			}
+			// generate single nbtrain file
+			else if(ops.containsKey("file")){
+				Path sourcefile = dir.resolve(ops.get("file").get(0));
+				singleTrainingDataGeneration(sourcefile);
+			}
+			// generate training set
+			else {
+				trainingSetGeneration(dir);
+			}			
+		}
+		// distribute library file
+		else if(cmd.equals("libraryIODef")){
+			distribute(tar);
+		}
 	}
 	
+	private static void parseCommandLineOptions(HashMap<String, ArrayList<String>> ops) {
+		// the net to use
+		String net = "default";
+		if(ops.containsKey("net")){
+			net = ops.get("net").get(0);
+		}
+		// setting up the net
+		NeuroBNet nbn = new DefaultPredicateSolverPredictionNet();
+		// future nets to come here
+		nb = new NeuroB(nbn.setSeed(0L).build());
+		
+		// load directory
+		if(ops.containsKey("dir")){
+			dir = Paths.get(ops.get("dir").get(0));
+		}
+		else {
+			// default
+			dir = Paths.get("prob_examples/public_examples/B/");
+		}
+		
+		if(ops.containsKey("tar")){
+			tar = Paths.get("tar");
+		}
+		else {
+			// default
+			tar = Paths.get("training_data/manual_call/");
+		}
+	}
+
 	private static void distribute(Path directory){
 		try (DirectoryStream<Path> stream = Files.newDirectoryStream(directory)) {
 			
@@ -112,8 +180,7 @@ public class NeuroBCli {
 		tsg.generateTrainingDataFile(source, target);
 	}
 	
-	private static void trainingSetGeneration(){
-		Path sourceDir = Paths.get("prob_examples/public_examples/B/");
+	private static void trainingSetGeneration(Path sourceDir){
 		Path targetDir = Paths.get("training_data/");
 		
 		nb.generateTrainingSet(sourceDir, targetDir);
