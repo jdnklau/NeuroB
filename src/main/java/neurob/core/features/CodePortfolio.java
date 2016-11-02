5dbdbb6543b8ca4ed9f93493d759e528ce769672
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2016-11-02 12:44:19 +0100

    Added code portfolio as feature class.
    Now one can create a BufferedImage from a given (a predicate or source code for example). This image shall serve as anonymous features one can compare his results with crafted features to.

---
+++ src/main/java/neurob/core/features/CodePortfolio.java
@@ -0,0 +1,81 @@
+package neurob.core.features;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.awt.image.WritableRaster;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class CodePortfolio {
+	private int size;
+	private BufferedImage image;
+	
+	/**
+	 * <p>Creates a CodePortfolio with the dimension of the given size squared
+	 * </p><p>
+	 * {@code new CodePortfolio(64); // Creates a 64*64 pixel image}
+	 * </p> 
+	 * @param dimensionSize
+	 */
+	public CodePortfolio(int dimensionSize, String code) {
+		size = dimensionSize;
+		
+		setData(code);
+	}
+	
+	/**
+	 * Generates internally an image of the code, that serves as input for a neural net
+	 * @param code
+	 */
+	private void setData(String code){
+		/*
+		 * The code is of size n^2.
+		 * 
+		 * Step 1: Create an image of size n*n and map each character of the code to a pixel in the image.
+		 * As the characters are a value in 0-255, they induce naturally a colour on the gray scale.
+		 * 
+		 * Step 2: Rescale the image to {@code size}*{@code size}. This may induce loss of information, 
+		 * especially if the scaling goes down, but keeps musters in the image.
+		 * 
+		 */
+		
+		// set up image
+		int rootCodeLength = (int) Math.floor(Math.sqrt(code.length()));
+		BufferedImage img = new BufferedImage(rootCodeLength, rootCodeLength, BufferedImage.TYPE_BYTE_GRAY);
+		WritableRaster raster = img.getRaster();
+		
+		// fill image
+		for(int y = 0; y < rootCodeLength; y++){
+			for(int x = 0; x < rootCodeLength; x++){
+				int px = code.charAt(x+y*rootCodeLength);
+				raster.setPixel(x, y, new int[]{px, px, px, px});
+			}
+		}
+		
+		// resize image
+		Image tmp = img.getScaledInstance(size, size, Image.SCALE_SMOOTH);
+		BufferedImage scaled = new BufferedImage(size, size, BufferedImage.TYPE_BYTE_GRAY);
+		
+		Graphics2D g2d = scaled.createGraphics();
+		g2d.drawImage(tmp,0,0,null);
+		g2d.dispose();
+		
+		image = scaled;
+		
+	}
+	
+	public void display(){
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 300);
+		
+		frame.getContentPane().add(new JLabel(new ImageIcon(image)));
+		
+		frame.setVisible(true);
+		
+	}
+}
