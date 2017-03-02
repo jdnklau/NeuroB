3e362ef5427a9fb01cc776830702f6490abbaf8e
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2017-03-02 18:00:14 +0100

    Predicate collector now collects before/after predicates and weakest preconditions and primed invariants.

--- src/main/java/neurob/training/generators/util/PredicateCollector.java
+++ src/main/java/neurob/training/generators/util/PredicateCollector.java
@@ -4,9 +4,19 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import de.be4.classicalb.core.parser.BParser;
 import de.be4.classicalb.core.parser.exceptions.BCompoundException;
 import de.be4.classicalb.core.parser.node.Start;
+import de.prob.animator.command.AbstractCommand;
+import de.prob.animator.command.BeforeAfterPredicateCommand;
+import de.prob.animator.command.PrimePredicateCommand;
+import de.prob.animator.command.WeakestPreconditionCommand;
+import de.prob.animator.domainobjects.ClassicalB;
+import de.prob.animator.domainobjects.EventB;
+import de.prob.animator.domainobjects.IEvalElement;
 import de.prob.model.classicalb.Assertion;
 import de.prob.model.classicalb.PrettyPrinter;
 import de.prob.model.classicalb.Property;
@@ -17,6 +27,7 @@ import de.prob.model.representation.Guard;
 import de.prob.model.representation.Invariant;
 import de.prob.statespace.StateSpace;
 import neurob.core.util.MachineType;
+import neurob.training.TrainingSetGenerator;
 
 public class PredicateCollector {
 	private ArrayList<String> invariants;
@@ -26,8 +37,13 @@ public class PredicateCollector {
 	private ArrayList<String> properties;
 	private ArrayList<String> assertions;
 	private ArrayList<String> theorems;
+	private ArrayList<String> beforeAfterPredicates;
+	private ArrayList<String> weakestPreconditions;
+	private ArrayList<String> primedInvariants;
 	
 	private MachineType machineType;
+	
+	private static final Logger log = LoggerFactory.getLogger(TrainingSetGenerator.class);
 	
 	public PredicateCollector(StateSpace ss){
 		preds = new ArrayList<String>();
@@ -37,13 +53,17 @@ public class PredicateCollector {
 		properties = new ArrayList<String>();
 		assertions = new ArrayList<String>();
 		theorems = new ArrayList<String>();
-		
-		collectPredicates(ss.getMainComponent());
-		
+		beforeAfterPredicates = new ArrayList<String>();
+		weakestPreconditions = new ArrayList<String>();
+		primedInvariants = new ArrayList<String>();
+
 		machineType = MachineType.getTypeFromStateSpace(ss);
+		
+		collectPredicates(ss);
 	}
 	
-	private void collectPredicates(AbstractElement comp){
+	private void collectPredicates(StateSpace ss){
+		AbstractElement comp = ss.getMainComponent();
 		// properties
 		for(Property x : comp.getChildrenOfType(Property.class)){
 			properties.add(x.getFormula().getCode());
@@ -55,6 +75,7 @@ public class PredicateCollector {
 				theorems.add(x.getFormula().getCode());
 			else
 				invariants.add(x.getFormula().getCode());
+			
 		}
 		
 		for(Assertion x : comp.getChildrenOfType(Assertion.class)){
@@ -74,6 +95,94 @@ public class PredicateCollector {
 		for(Axiom x : comp.getChildrenOfType(Axiom.class)){
 			axioms.add(x.getFormula().getCode());
 		}
+		
+		// weakest preconditions
+		for(BEvent x : comp.getChildrenOfType(BEvent.class)){
+			if(x.getName().equals("INITIALISATION"))
+				continue; // None for initialisation
+			
+			WeakestPreconditionCommand wpcc;
+			for(String inv : invariants){
+				IEvalElement invariant;
+				switch(machineType){
+				case EVENTB:
+					invariant = new EventB(inv);
+					break;
+				default:
+				case CLASSICALB:
+					invariant = new ClassicalB(inv);
+					break;
+				}				
+				
+				wpcc = new WeakestPreconditionCommand(x.getName(), invariant);
+				try{
+					ss.execute(wpcc);
+					weakestPreconditions.add(wpcc.getWeakestPrecondition().getCode());
+				} catch(Exception e) {
+					log.warn("\tCould not build weakest precondition for {} by event {}: {}", inv, x.getName(), e.getMessage(), e);
+				}
+				
+				
+			}
+			
+			// weakest pre condition for all invariants
+			IEvalElement invariant;
+			String inv = String.join(" & ", invariants);
+			switch(machineType){
+			case EVENTB:
+				invariant = new EventB(inv);
+				break;
+			default:
+			case CLASSICALB:
+				invariant = new ClassicalB(inv);
+				break;
+			}				
+			
+			wpcc = new WeakestPreconditionCommand(x.getName(), invariant);
+			try{
+				ss.execute(wpcc);
+				weakestPreconditions.add(wpcc.getWeakestPrecondition().getCode());
+			} catch(Exception e) {
+				log.warn("\tCould not build weakest precondition for {} by event {}: {}", inv, x.getName(), e.getMessage(), e);
+			}
+			
+		}
+		
+		// Before/After predicates
+		for(BEvent x : comp.getChildrenOfType(BEvent.class)){
+			if(x.getName().equals("INITIALISATION"))
+				continue; // None for initialisation
+			
+			BeforeAfterPredicateCommand bapc = new BeforeAfterPredicateCommand(x.getName());
+			try{
+				ss.execute(bapc);
+				beforeAfterPredicates.add(bapc.getBeforeAfterPredicate().getCode());
+			} catch(Exception e) {
+				log.warn("\tCould not build Before After Predicate for event {}: {}", x.getName(), e.getMessage(), e);
+			}
+		
+		}
+
+		for(String inv : invariants){
+			IEvalElement invariant;
+			switch(machineType){
+			case EVENTB:
+				invariant = new EventB(inv);
+				break;
+			default:
+			case CLASSICALB:
+				invariant = new ClassicalB(inv);
+				break;
+			}
+			
+			PrimePredicateCommand ppc = new PrimePredicateCommand(invariant);
+			try{
+				ss.execute(ppc);
+				primedInvariants.add(ppc.getPrimedPredicate().getCode());
+			}catch(Exception e) {
+				log.warn("\tCould not build primed invariant from {}: {}", inv, e.getMessage(), e);
+			}
+		}
 			
 	}
 
@@ -82,6 +191,9 @@ public class PredicateCollector {
 	public ArrayList<String> getProperties(){ return properties; }
 	public ArrayList<String> getAssertions(){ return assertions; }
 	public ArrayList<String> getTheorems(){ return theorems; }
+	public ArrayList<String> getBeforeAfterPredicates(){ return beforeAfterPredicates; }
+	public ArrayList<String> getWeakestPreConditions(){ return weakestPreconditions; }
+	public ArrayList<String> getPrimedInvariants(){ return primedInvariants; }
 
 	/**
 	 * Modifies an ArrayList of predicates to have only Numbers of infinite domains.
