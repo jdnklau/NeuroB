6a0e16640534e2d2f60b19d32976043767228aa0
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2017-03-14 17:50:51 +0100

    Added parser handling to classical B predicate features.

--- src/main/java/neurob/core/features/PredicateFeatures.java
+++ src/main/java/neurob/core/features/PredicateFeatures.java
@@ -1,11 +1,14 @@
 package neurob.core.features;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.nd4j.linalg.api.ndarray.INDArray;
 
-import de.be4.classicalb.core.parser.exceptions.BCompoundException;
+import de.be4.classicalb.core.parser.BParser;
+import de.be4.eventbalg.core.parser.EventBParser;
 import neurob.core.features.interfaces.FeatureGenerator;
 import neurob.core.features.util.ClassicalBPredicateFeatureData;
 import neurob.core.features.util.PredicateFeatureData;
@@ -17,6 +20,7 @@ public class PredicateFeatures implements FeatureGenerator {
 	public static final int featureDimension = 17; // Dimension of feature vectors
 	private ArrayList<String> features; // The stored features
 	private MachineType machineType;
+	private BParser bParser;
 	
 	
 	public PredicateFeatures() {
@@ -24,13 +28,31 @@ public class PredicateFeatures implements FeatureGenerator {
 	}
 	
 	public PredicateFeatures(MachineType mt){
-		features = new ArrayList<String>();
+		reset();
 		machineType = mt;
 	}
 
 	@Override
 	public void reset(){
 		features = new ArrayList<String>();
+		bParser = new BParser();
+	}
+	
+	public void setMachine(Path machineFile) throws NeuroBException{
+		String fileName = machineFile.getFileName().toString();
+		if(fileName.endsWith(".mch")) {
+			try {
+				bParser = new BParser(machineFile.toString());
+				bParser.parseFile(machineFile.toFile(), false);
+			} catch (IOException e) {
+				throw new NeuroBException("Could not access source file "+machineFile, e);
+			} catch (Exception e) {
+				throw new NeuroBException("Could not parse source file "+machineFile, e);
+			}
+			machineType = MachineType.CLASSICALB;
+		} else if(fileName.endsWith(".bcm")){
+			machineType = MachineType.EVENTB;
+		}
 	}
 	
 	private PredicateFeatureData generatePredicateFeatureData(String predicate) throws NeuroBException{
@@ -39,7 +61,7 @@ public class PredicateFeatures implements FeatureGenerator {
 			switch(machineType){
 			case CLASSICALB:
 			default: // defaulting to classical b
-				pfd = new ClassicalBPredicateFeatureData(predicate);
+				pfd = new ClassicalBPredicateFeatureData(predicate, bParser);
 			}
 		} catch (NeuroBException e) {
 			throw new NeuroBException("Could not generate feature string from predicate: "+predicate, e);
@@ -75,7 +97,6 @@ public class PredicateFeatures implements FeatureGenerator {
 	
 	@Override
 	public int getFeatureDimension() {
-		// TODO Auto-generated method stub
 		return featureDimension;
 	}
 
--- src/main/java/neurob/core/features/util/ClassicalBPredicateFeatureData.java
+++ src/main/java/neurob/core/features/util/ClassicalBPredicateFeatureData.java
@@ -1,27 +1,36 @@
 package neurob.core.features.util;
 
 import de.be4.classicalb.core.parser.BParser;
-import de.be4.classicalb.core.parser.exceptions.BCompoundException;
 import de.be4.classicalb.core.parser.node.Start;
 import neurob.exceptions.NeuroBException;
 
 public class ClassicalBPredicateFeatureData extends PredicateFeatureData {
+	private BParser parser;
 	
-	public ClassicalBPredicateFeatureData(String predicate) throws NeuroBException{
-		this();
+	public ClassicalBPredicateFeatureData(String predicate, BParser parser) throws NeuroBException {
+		this(parser);
 		collectData(predicate);
 	}
 	
-	public ClassicalBPredicateFeatureData() {
+	public ClassicalBPredicateFeatureData(String predicate) throws NeuroBException{
+		this(predicate, new BParser());
+	}
+	
+	public ClassicalBPredicateFeatureData(BParser parser) {
 		super();
+		this.parser = parser;
+	}
+	
+	public ClassicalBPredicateFeatureData(){
+		this(new BParser());
 	}
 	
 	@Override
 	public void collectData(String predicate) throws NeuroBException {
 		Start ast;
 		try {
-			ast = BParser.parse(BParser.FORMULA_PREFIX+" "+predicate);
-		} catch (BCompoundException e) {
+			ast = parser.parse(BParser.PREDICATE_PREFIX+" "+predicate, false, parser.getContentProvider());
+		} catch (Exception e) {
 			throw new NeuroBException("Could not collect feature data from predicate "+predicate, e);
 		}
 		
