6828d8e305dc9d286a22116c4a00323a65dded42
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2017-03-16 10:52:03 +0100

    Added test case to verify necessity of using the parser for predicate features.

--- src/test/java/neurob/tests/training/pdumps/PredicateDumperTranslationTest.java
+++ src/test/java/neurob/tests/training/pdumps/PredicateDumperTranslationTest.java
@@ -2,12 +2,21 @@ package neurob.tests.training.pdumps;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.junit.Test;
 
 import neurob.core.features.PredicateFeatures;
 import neurob.core.features.interfaces.FeatureGenerator;
 import neurob.core.util.SolverType;
 import neurob.exceptions.NeuroBException;
+import neurob.training.TrainingSetGenerator;
 import neurob.training.generators.interfaces.PredicateDumpTranslator;
 import neurob.training.generators.labelling.SolverClassificationGenerator;
 import neurob.training.generators.labelling.SolverSelectionGenerator;
@@ -15,6 +24,9 @@ import neurob.training.generators.labelling.SolverTimerGenerator;
 
 public class PredicateDumperTranslationTest {
 	private final String predicateDump = "-1,-1,2525884156:x:NATURAL & y:NATURAL & z:INTEGER & y<x & z<20 <=> x<y";
+	
+	private final Path schleusenMch = Paths.get("src/test/resources/Schleusen.mch");
+	private final Path schleusenPdump = Paths.get("src/test/resources/Schleusen.pdump");
 	
 	@Test
 	public void SolverClassificationPredicateDumpTranslationTest() throws NeuroBException{
@@ -60,5 +72,44 @@ public class PredicateDumperTranslationTest {
 		
 		assertEquals("Predicate dump translation for solver selection does not match", expected, actual);
 	}
-
+	
+	@Test
+	public void TranslateToPredicateFeaturesWithParsedFileTest() throws NeuroBException, IOException{
+		PredicateDumpTranslator pdt = new SolverClassificationGenerator(SolverType.PROB);
+		FeatureGenerator fg = new PredicateFeatures(schleusenMch);
+		
+		List<String> lines = Files.lines(schleusenPdump).collect(Collectors.toList());
+		
+		String expected = "0,2,1,0,3,0,0,0,2,1,0,8,0,0,8,1,0,1";
+		String actual;
+		try{
+			actual = pdt.translateToCSVDataString(fg, lines.get(0));
+			assertEquals("Feature representation does not match", expected, actual);
+		} catch(NeuroBException e){
+			assertTrue(e.getMessage(), false);
+		}
+		
+	}
+	
+	/**
+	 * This test is a sanity check of {@link #TranslateToPredicateFeaturesWithParsedFileTest()}.
+	 * <p>
+	 * It checks whether parsing the file or not leads to different results.
+	 * @throws NeuroBException
+	 * @throws IOException
+	 */
+	@Test
+	public void TranslateToPredicateFeaturesWithoutParsedFileTest() throws NeuroBException, IOException{
+		PredicateDumpTranslator pdt = new SolverClassificationGenerator(SolverType.PROB);
+		FeatureGenerator fg = new PredicateFeatures();
+		
+		List<String> lines = Files.lines(schleusenPdump).collect(Collectors.toList());
+		
+		try{
+			pdt.translateToCSVDataString(fg, lines.get(0));
+			assertTrue("Could unexpectedly parse the predicate given.", false);
+		} catch(NeuroBException e){
+			// nothing
+		}
+	}
 }
