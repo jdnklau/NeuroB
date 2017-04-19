8247f965f710304e20ee210318c574331ed3e3ec
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2017-04-19 16:07:17 +0200

    First step for problem type specific model evaluation.
    Added new class to evaluate models with. ModelEvaluation and its subclasses are meant to wrap the DL4J evaluations and maybe enhance their output.

---
+++ src/main/java/neurob/training/statistics/ClassificationModelEvaluation.java
@@ -0,0 +1,102 @@
+package neurob.training.statistics;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.deeplearning4j.eval.Evaluation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import neurob.core.nets.NeuroBNet;
+import neurob.exceptions.NeuroBException;
+import neurob.training.statistics.interfaces.ModelEvaluation;
+
+public class ClassificationModelEvaluation extends ModelEvaluation<Evaluation> {
+	protected BufferedWriter epochCSV;
+	private static final Logger log = LoggerFactory.getLogger(ClassificationModelEvaluation.class);
+
+	public ClassificationModelEvaluation(NeuroBNet model, Path csvFile) throws IOException {
+		super(model, csvFile);
+	}
+
+	@Override
+	protected void setup(Path csv) throws IOException {
+		epochCSV = Files.newBufferedWriter(csv);
+		
+		// setting up csv header
+		List<String> columns = new ArrayList<>();
+		columns.add("Epoch");
+		// training set stats
+		columns.add("Training Accuracy");
+		columns.add("Training Precision");
+		columns.add("Training Recall");
+		columns.add("Training F1 Score");
+		// test set stats
+		columns.add("Test Accuracy");
+		columns.add("Test Precision");
+		columns.add("Test Recall");
+		columns.add("Test F1 Score");
+		
+		epochCSV.write(String.join(",", columns));
+		epochCSV.newLine();
+		epochCSV.flush();
+	}
+
+	@Override
+	public void evaluateAfterEpoch(Path trainingSet, Path testSet) throws NeuroBException {
+		Evaluation trainEval;
+		Evaluation testEval;
+		try {
+			trainEval = evaluateModel(trainingSet);
+			testEval = evaluateModel(testSet);
+		} catch (IOException | InterruptedException e) {
+			throw new NeuroBException("Could not evaluate training or test set.", e);
+		}
+		
+		// log results
+		logEvaluation("Training", trainEval);
+		logEvaluation("Testing", testEval);
+		
+		// set up line of csv
+		List<String> columns = new ArrayList<>();
+		columns.add(Integer.toString(++epochsSeen)); // NOTE: increasing number of seen epochs here
+		columns.addAll(partialCSVEntries(trainEval));
+		columns.addAll(partialCSVEntries(testEval));
+		
+		// evaluate best epoch
+		// TODO: actually implement a metric to check if new evaluation performed better.
+		bestEpochSeen = epochsSeen; 
+		log.info("\tBest epoch thus far: #{}", bestEpochSeen);
+	}
+	
+	private void logEvaluation(String caption, Evaluation testEval) {
+		log.info("\t{} stats - Accuracy: {}; Precision: {}; Recall: {}; F1 score: {}",
+				caption,
+				testEval.accuracy(),
+				testEval.precision(),
+				testEval.recall(),
+				testEval.f1());
+	}
+
+	protected List<String> partialCSVEntries(Evaluation eval){
+		List<String> entries = new ArrayList<>();
+		// test set stats
+		entries.add(Double.toString(eval.accuracy()));
+		entries.add(Double.toString(eval.precision()));
+		entries.add(Double.toString(eval.recall()));
+		entries.add(Double.toString(eval.f1()));
+		
+		return entries;
+	}
+
+	@Override
+	public Evaluation evaluateModel(Path testSet) throws IOException, InterruptedException {
+		Evaluation eval = new Evaluation(nbn.getClassificationSize());
+		return evaluateModel(testSet, eval);
+	}
+
+}
---
+++ src/main/java/neurob/training/statistics/interfaces/ModelEvaluation.java
@@ -0,0 +1,86 @@
+package neurob.training.statistics.interfaces;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import org.deeplearning4j.eval.IEvaluation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
+
+import neurob.core.nets.NeuroBNet;
+import neurob.exceptions.NeuroBException;
+
+@SuppressWarnings("rawtypes")
+public abstract class ModelEvaluation<T extends IEvaluation> {
+	protected final NeuroBNet nbn;
+	
+	/**
+	 * Number of epochs that were evaluated.
+	 */
+	protected int epochsSeen;
+	
+	/**
+	 * The best evaluated epoch seen thus far.
+	 */
+	protected int bestEpochSeen;
+	
+	public ModelEvaluation(NeuroBNet model, Path csvFile) throws IOException {
+		this.nbn = model;
+		epochsSeen = 0;
+		bestEpochSeen = -1;
+		setup(csvFile);
+	}
+	
+	/**
+	 * Sets up the statistics file as CSV with header line. 
+	 * @param csv CSV to be generated
+	 * @throws IOException
+	 */
+	protected abstract void setup(Path csv) throws IOException;
+
+	public int getEpochsSeen(){return epochsSeen;}
+	public int getBestEpochSeen(){return bestEpochSeen;}
+	
+	/**
+	 * Increases internal epoch counter by 1, evaluates the model on the test set
+	 * and checks whether the new epoch reached the best performance on the model or not.
+	 * <p>
+	 * Saves the generated results as a line to the csv file.
+	 * @param trainingSet Location of training set, to calculate training error
+	 * @param testSet Location of test set, to calculate test error
+	 * @throws NeuroBException
+	 */
+	public abstract void evaluateAfterEpoch(Path trainingSet, Path testSet) throws NeuroBException;
+	
+	/**
+	 * Evaluates the model on the given test set.
+	 * @param testSet
+	 * @return Eval object, that evaluated the given model
+	 */
+	public abstract T evaluateModel(Path testSet) throws IOException, InterruptedException;
+	
+	/**
+	 * Evaluates the model in the given test set and feeds the data into the {@link IEvaluation eval}
+	 * object.
+	 * @param testSet
+	 * @param eval
+	 * @return
+	 * @throws IOException
+	 * @throws InterruptedException
+	 */
+	protected T evaluateModel(Path testSet, T eval) throws IOException, InterruptedException{
+		int batchSize = 100;
+		DataSetIterator iterator = nbn.getDataSetIterator(testSet, batchSize);
+		nbn.applyNormalizer(iterator); //Apply normalization learned from training set
+
+		// evaluate test set
+		while(iterator.hasNext()){
+			DataSet testData = iterator.next();
+        	INDArray output = nbn.output(testData.getFeatureMatrix());        	
+        	eval.eval(testData.getLabels(), output);
+		}
+		
+		return eval;
+	}
+}
