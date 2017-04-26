004446af9f2c432f7e1b62b06a3ded4f397e8744
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2017-04-26 17:04:57 +0200

    Start work on hyper parameter search utility
    Not finished yet; lacks evaluation of what model performed best.
    Will also probably crash if used for non-feedforward models.

---
+++ src/main/java/neurob/training/HyperParameterSearch.java
@@ -0,0 +1,116 @@
+package neurob.training;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.deeplearning4j.arbiter.DL4JConfiguration;
+import org.deeplearning4j.arbiter.optimize.api.CandidateGenerator;
+import org.deeplearning4j.eval.IEvaluation;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+
+import neurob.core.NeuroB;
+import neurob.core.features.interfaces.FeatureGenerator;
+import neurob.core.nets.NeuroBNet;
+import neurob.training.generators.interfaces.LabelGenerator;
+import neurob.training.statistics.interfaces.ModelEvaluation;
+
+public class HyperParameterSearch
+	<T extends CandidateGenerator<DL4JConfiguration>> {
+	private T candidateGenerator;
+	private Path savePath;
+	private int bestPerformingIndex;
+	private ModelEvaluation<IEvaluation> modelEvaluation = null;
+	private FeatureGenerator featureGenerator;
+	private LabelGenerator labelGenerator;
+
+	public HyperParameterSearch(T candidateGenerator,
+			FeatureGenerator featureGen, LabelGenerator labelGen) {
+		this(candidateGenerator, 
+				featureGen, labelGen,
+				Paths.get("random_search/")
+				.resolve(ZonedDateTime.now()
+						.format(DateTimeFormatter.ISO_INSTANT))
+				);
+	}
+	
+	public HyperParameterSearch(T candidateGenerator,
+			FeatureGenerator featureGen, LabelGenerator labelGen,
+			Path modelDirectory){
+		this.candidateGenerator = candidateGenerator;
+		savePath = modelDirectory;
+		bestPerformingIndex = -1;
+		
+		featureGenerator = featureGen;
+		labelGenerator = labelGen;
+	}
+	
+	/**
+	 * Trains multiple models, saves them to disk and reports
+	 * the index of the best performing model.
+	 * <p>
+	 * In the target directory, a subdirectory will be created for each model.
+	 * Those subdirectories are enumerated from 0 onwards, being the model
+	 * index.
+	 * <p>
+	 * It is advised to use a relatively small epoch number, as hyperparameter
+	 * search is only meant to find hyperparameter settings to use for
+	 * following, longer training.
+	 * 
+	 * @param numModels Maximum number of models to train
+	 * @param trainSource Location of training set
+	 * @param testSource Location of test set
+	 * @param numEpochs Number of epochs to train each model
+	 * @return index of best performing model
+	 * @throws InterruptedException 
+	 * @throws IOException 
+	 */
+	public int trainModels(int numModels, 
+			Path trainSource, Path testSource, int numEpochs)
+			throws IOException, InterruptedException{
+		/*
+		 * TODO
+		 * - [x] get candidates
+		 * - [x] train them
+		 * - [x] save them (automatically done by NeuroB#train call)
+		 * - [ ] get final evaluation for comparison
+		 * - [ ] update best model
+		 * - [ ] return index of best performing model  
+		 */
+		// Train and evaluate up to numModels candidates
+		for(int i=0; i<numModels && candidateGenerator.hasMoreCandidates();
+				i++){
+			// get candidate
+			MultiLayerNetwork model = new MultiLayerNetwork(
+					candidateGenerator.getCandidate()
+					.getValue().getMultiLayerConfiguration());
+			// make neuroB net instance
+			// TODO: distinguish between NN, CNN, RNN, etc
+        	NeuroBNet nbn = new NeuroBNet(model, 
+        			featureGenerator, labelGenerator);
+        	Path modelSavePath = 
+        			savePath.resolve(nbn.getDataPathName())
+        			.resolve(Integer.toString(i));
+			// set up NeuroB
+			NeuroB nb = new NeuroB(nbn, modelSavePath);
+			
+			// train model
+			nb.train(trainSource, testSource, numEpochs);
+			
+		}
+		
+		
+		return bestPerformingIndex;
+	}
+	
+	public int getBestPerformingIndex() {
+		return bestPerformingIndex;
+	}
+
+	public ModelEvaluation<IEvaluation> getModelEvaluation() {
+		return modelEvaluation;
+	}
+
+}
