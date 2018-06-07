75f69a9482c49d4262d12dff789ea79fbc43bc3c
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2018-06-07 16:23:36 +0200

    Add Training Data Generation API

--- core/src/main/java/de/hhu/stups/neurob/training/data/TrainingSample.java
+++ core/src/main/java/de/hhu/stups/neurob/training/data/TrainingSample.java
@@ -3,15 +3,22 @@ package de.hhu.stups.neurob.training.data;
 import de.hhu.stups.neurob.core.api.Features;
 import de.hhu.stups.neurob.core.api.Labelling;
 
+import java.nio.file.Path;
+
 public class TrainingSample<F extends Features, L extends Labelling> {
 
     private final F features;
-
     private final L labelling;
+    private final Path sourceFile;
 
     public TrainingSample(F features, L labelling) {
+        this(features, labelling, null);
+    }
+
+    public TrainingSample(F features, L labelling, Path sourceFile) {
         this.features = features;
         this.labelling = labelling;
+        this.sourceFile = sourceFile;
     }
 
     public F getFeatures() {
---
+++ core/src/main/java/de/hhu/stups/neurob/training/formats/TrainingDataFormat.java
@@ -0,0 +1,49 @@
+package de.hhu.stups.neurob.training.formats;
+
+import de.hhu.stups.neurob.core.api.Features;
+import de.hhu.stups.neurob.core.api.Labelling;
+import de.hhu.stups.neurob.training.data.TrainingSample;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Stream;
+
+public interface TrainingDataFormat {
+
+    /**
+     * Generates the target location path to be written to at training data
+     * generation time.
+     *
+     * @param sourceFile
+     * @param targetDirectory
+     *
+     * @return Path of the target for training data generation
+     */
+    Path getTargetLocation(Path sourceFile, Path targetDirectory);
+
+    /**
+     * Writes the given <code>samples</code> into the target location in the
+     * respective format.
+     *
+     * @param samples List of training data samples
+     * @param targetDirectory location to populate with training data
+     */
+    default
+    <F extends Features, L extends Labelling>
+    void writeSamples(List<TrainingSample<F, L>> samples,
+            Path targetDirectory) {
+        writeSamples(samples.stream(), targetDirectory);
+    }
+
+    /**
+     * Writes the given <code>samples</code> into the target location in the
+     * respective format.
+     *
+     * @param samples Stream of training data samples
+     * @param targetDirectory location to populate with training data
+     */
+    <F extends Features, L extends Labelling>
+    void writeSamples(Stream<TrainingSample<F, L>> samples,
+            Path targetDirectory);
+
+}
