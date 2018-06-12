268249f8851880a7b1b8a81f3473e2b571f5fbf8
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2018-06-12 17:00:42 +0200

    Change PredicateFeatures class to interface
    Feature generation of predicate features is still implementation
    dependent. Defining PredicateFeatures to be an interface allows to add
    its functionality to classes that already need to extend from another
    base class.

--- core/src/main/java/de/hhu/stups/neurob/core/features/PredicateFeatures.java
+++ core/src/main/java/de/hhu/stups/neurob/core/features/PredicateFeatures.java
@@ -3,15 +3,7 @@ package de.hhu.stups.neurob.core.features;
 /**
  * Features over B-predicates.
  */
-public abstract class PredicateFeatures implements Features {
+public interface PredicateFeatures extends Features {
 
-    protected final String predicate;
-
-    public PredicateFeatures(String predicate) {
-        this.predicate = predicate;
-    }
-
-    public String getPredicate() {
-        return predicate;
-    }
+    String getPredicate();
 }
