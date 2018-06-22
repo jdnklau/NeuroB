15598207eadbc005c0846d0cebaa65de6548f7d3
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2018-06-22 14:25:31 +0200

    Add DecisionTimings labelling
    Provide class for labelling a predicate with respect to the time needed
    per solver to decide it.

---
+++ core/src/main/java/de/hhu/stups/neurob/core/labelling/DecisionTimings.java
@@ -0,0 +1,237 @@
+package de.hhu.stups.neurob.core.labelling;
+
+import de.hhu.stups.neurob.core.api.backends.Backend;
+import de.hhu.stups.neurob.core.exceptions.FormulaException;
+import de.hhu.stups.neurob.core.exceptions.LabelCreationException;
+import de.prob.statespace.StateSpace;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Labels a predicate over different backends with how long each backend took
+ * to give a decision.
+ * Timings are given in nano seconds. Negative values indicate non-decidability
+ * with respect to the respective backend.
+ * <p>
+ * If in the constructor no timeout values are set, the defaults are taken,
+ * dictated by {@link DecisionTimings#defaultTimeout}
+ * and {@link DecisionTimings#defaultTimeoutUnit}.
+ */
+public class DecisionTimings implements PredicateLabelling {
+
+    private final String predicate;
+    private final Double[] timingArray;
+    /** Number of times each timing is run; final timing is average of all calls */
+    private final int sampleSize;
+
+    // Time out how long a predicate will maximally run in evaluation.
+    private final Long timeOut;
+    private final TimeUnit timeUnit;
+
+    /** Default time out set for predicate evaluation. */
+    public final static Long defaultTimeout = 20L;
+    /** Unit of defaultTimeout */
+    public final static TimeUnit defaultTimeoutUnit = TimeUnit.SECONDS;
+
+    private final Backend[] usedBackends;
+    private Map<Backend, Double> timings;
+
+    private final int labellingDimension;
+
+    private static final Logger log =
+            LoggerFactory.getLogger(DecisionTimings.class);
+
+    /**
+     * Sets the default timeout for predicate evaluation.
+     *
+     * @param predicate
+     * @param sampleSize Number of times each timing is run; final time
+     *         is taken from the average.
+     * @param stateSpace StateSpace over which the predicate shall be
+     *         decided.
+     * @param backends List of backend for which the labelling is
+     *         created.
+     */
+    public DecisionTimings(String predicate, int sampleSize,
+            StateSpace stateSpace,
+            Backend... backends)
+            throws LabelCreationException {
+        this(predicate, sampleSize, defaultTimeout, defaultTimeoutUnit,
+                stateSpace, backends);
+    }
+
+    /**
+     * @param predicate
+     * @param sampleSize Number of times each timing is run; final time
+     *         is taken from the average.
+     * @param stateSpace StateSpace over which the predicate shall be
+     *         decided.
+     * @param backends List of backend for which the labelling is
+     *         created.
+     */
+    public DecisionTimings(String predicate, int sampleSize,
+            Long timeOut, TimeUnit timeOutUnit,
+            StateSpace stateSpace, Backend... backends)
+            throws LabelCreationException {
+        this.predicate = predicate;
+        this.sampleSize = sampleSize;
+        this.usedBackends = backends;
+
+        this.labellingDimension = backends.length;
+
+        this.timeOut = timeOut;
+        this.timeUnit = timeOutUnit;
+        this.timings = createTimings(stateSpace, backends);
+
+        this.timingArray = new Double[labellingDimension];
+        for (int i = 0; i < labellingDimension; i++) {
+            timingArray[i] = timings.get(backends[i]);
+        }
+    }
+
+    private Map<Backend, Double> createTimings(StateSpace ss, Backend... backends)
+            throws LabelCreationException {
+        Map<Backend, Double> timings = new HashMap<>();
+
+        // TODO: set timeout inside of state space as well
+
+        for (Backend backend : backends) {
+            timings.put(backend, sampleTimings(ss, backend));
+        }
+
+        return timings;
+    }
+
+    private Double sampleTimings(StateSpace ss, Backend backend)
+            throws LabelCreationException {
+        Double sampled = 0.;
+        for (int i = 0; i < sampleSize; i++) {
+            Long timing = null;
+            try {
+                timing = backend.measureEvalTime(predicate, ss);
+            } catch (FormulaException e) {
+                throw new LabelCreationException(
+                        "Could not create timing sample #" + i
+                        + " for " + backend.toString() + "over predicate "
+                        + predicate,
+                        e);
+            }
+            // stop if already not decidable
+            if (timing < 0) {
+                sampled = -1.;
+                break;
+            }
+            sampled += timing;
+        }
+
+        return sampled / sampleSize;
+    }
+
+    @Override
+    public String getPredicate() {
+        return predicate;
+    }
+
+    /**
+     * Returns an array consisting of the measured timings for each backend in
+     * use.
+     * <p>
+     * The order of entries matches the order of backends returned by
+     * {@link #getUsedBackends()}.
+     * </p>
+     *
+     * @return
+     *
+     * @see #getTiming(Backend)
+     */
+    @Override
+    public Double[] getLabellingArray() {
+        return timingArray;
+    }
+
+    /**
+     * Returns the time it took for the given backend to decide the predicate.
+     * If the queried backend is not part of the used backends, null is
+     * returned.
+     *
+     * @param backend
+     *
+     * @return The decision time needed from the respective backend.
+     */
+    public Double getTiming(Backend backend) {
+        return timings.getOrDefault(backend, null);
+    }
+
+    @Override
+    public int getLabellingDimension() {
+        return labellingDimension;
+    }
+
+    /**
+     * Returns number of times the predicate was queried for each backend. The
+     * average
+     * is taken for the final labelling.
+     *
+     * @return
+     */
+    public int getSampleSize() {
+        return sampleSize;
+    }
+
+    /**
+     * Returns an array of used backends for which the times were generated.
+     *
+     * @return
+     */
+    public Backend[] getUsedBackends() {
+        return usedBackends;
+    }
+
+    public static class Generator
+            implements PredicateLabelGenerating<DecisionTimings> {
+
+        private final int sampleSize;
+        private final Backend[] backends;
+        private final Long timeout;
+        private final TimeUnit timeoutUnit;
+
+        /**
+         * Sets the timeout for predicate evaluation to default of
+         * {@link DecisionTimings}.
+         *
+         * @param sampleSize Number of times each backend runs
+         * @param backends List of backends for which the timings are
+         *         calculated.
+         */
+        public Generator(int sampleSize, Backend... backends) {
+            this(sampleSize, defaultTimeout, defaultTimeoutUnit, backends);
+        }
+
+        /**
+         * @param sampleSize Number of times each backend runs
+         * @param backends List of backends for which the timings are
+         *         calculated.
+         * @param timeout
+         * @param timeoutUnit
+         */
+        public Generator(int sampleSize, Long timeout, TimeUnit timeoutUnit,
+                Backend... backends) {
+            this.sampleSize = sampleSize;
+            this.backends = backends;
+            this.timeout = timeout;
+            this.timeoutUnit = timeoutUnit;
+        }
+
+        @Override
+        public DecisionTimings generate(String predicate, StateSpace ss)
+                throws LabelCreationException {
+            return new DecisionTimings(predicate, sampleSize,
+                    timeout, timeoutUnit, ss, backends);
+        }
+    }
+
+}
---
+++ core/src/test/java/de/hhu/stups/neurob/core/labelling/DecisionTimingsTest.java
@@ -0,0 +1,301 @@
+package de.hhu.stups.neurob.core.labelling;
+
+import de.hhu.stups.neurob.core.api.backends.Backend;
+import de.hhu.stups.neurob.core.exceptions.FormulaException;
+import de.hhu.stups.neurob.core.exceptions.LabelCreationException;
+import de.prob.animator.command.CbcSolveCommand.Solvers;
+import de.prob.statespace.StateSpace;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class DecisionTimingsTest {
+
+    private StateSpace stateSpace;
+    private Backend prob;
+    private Backend kodkod;
+    private Backend z3;
+    private Backend smt_supported_interpreter;
+
+    @BeforeEach
+    public void mockStateSpace() {
+        stateSpace = mock(StateSpace.class);
+    }
+
+    @BeforeEach
+    public void mockBackEnds() {
+        prob = mock(Backend.class);
+        doReturn(Solvers.PROB).when(prob).toCbcEnum();
+
+        kodkod = mock(Backend.class);
+        doReturn(Solvers.KODKOD).when(kodkod).toCbcEnum();
+
+        z3 = mock(Backend.class);
+        doReturn(Solvers.Z3).when(z3).toCbcEnum();
+
+        smt_supported_interpreter = mock(Backend.class);
+        doReturn(Solvers.SMT_SUPPORTED_INTERPRETER)
+                .when(smt_supported_interpreter).toCbcEnum();
+    }
+
+    @Test
+    public void shouldUseProBAndZ3Backends() throws LabelCreationException {
+        DecisionTimings timings = new DecisionTimings("predicate",
+                1, stateSpace, prob, z3);
+
+        Backend[] usedBackends = timings.getUsedBackends();
+
+        assertAll("Using ProB and Z3 backends",
+                () -> assertEquals(Solvers.PROB, usedBackends[0].toCbcEnum(),
+                        "ProB backend not at index 0"),
+                () -> assertEquals(Solvers.Z3, usedBackends[1].toCbcEnum(),
+                        "ProB backend not at index 0"),
+                () -> assertEquals(2, usedBackends.length,
+                        "Number of backends does not match"));
+    }
+
+    @Test
+    public void shouldReturn1AsLabellingDimensionWhenOnlyOneBackEndProvided()
+            throws LabelCreationException {
+        DecisionTimings timings = new DecisionTimings("predicate",
+                1, stateSpace, prob);
+
+        int expected = 1;
+        int actual = timings.getLabellingDimension();
+
+        assertEquals(expected, actual,
+                "Labelling dimension does not match");
+    }
+
+    @Test
+    public void shouldReturn1AsSampleSizeWhen1WasSpecified()
+            throws LabelCreationException {
+        DecisionTimings timings = new DecisionTimings("predicate",
+                1, stateSpace, prob);
+
+        int expected = 1;
+        int actual = timings.getSampleSize();
+
+        assertEquals(expected, actual,
+                "Sampling size does not match");
+    }
+
+    @Test
+    public void shouldReturnPredicate() throws Exception {
+        DecisionTimings timings = new DecisionTimings("predicate",
+                1, stateSpace, prob);
+
+        assertEquals("predicate", timings.getPredicate());
+    }
+
+    @Test
+    public void shouldReturn3AsSampleSizeWhen3WasSpecified()
+            throws LabelCreationException {
+        DecisionTimings timings = new DecisionTimings("predicate",
+                3, stateSpace, prob);
+
+        int expected = 3;
+        int actual = timings.getSampleSize();
+
+        assertEquals(expected, actual,
+                "Sampling size does not match");
+    }
+
+    @Test
+    public void shouldReturn3AsLabellingDimensionWhenThreeBackendsAreProvided()
+            throws LabelCreationException {
+        DecisionTimings timings = new DecisionTimings("predicate",
+                1, stateSpace, prob, kodkod, z3);
+
+        int expected = 3;
+        int actual = timings.getLabellingDimension();
+
+        assertEquals(expected, actual,
+                "Labelling dimension does not match");
+    }
+
+    @Test
+    public void shouldSampleOnce() throws Exception {
+        // Stub ProB
+        when(prob.measureEvalTime("predicate", stateSpace))
+                .thenReturn(1L)
+                .thenReturn(100L);
+
+        DecisionTimings timings = new DecisionTimings("predicate", 1, stateSpace, prob);
+
+        Double expected = 1.;
+        Double actual = timings.getTiming(prob);
+
+        assertEquals(expected, actual, 1e-5,
+                "Measured time should be 1ns");
+    }
+
+    @Test
+    public void shouldSampleThrice() throws Exception {
+        // Stub ProB
+        when(prob.measureEvalTime("predicate", stateSpace))
+                .thenReturn(1L)
+                .thenReturn(2L)
+                .thenReturn(6L);
+
+        DecisionTimings timings = new DecisionTimings("predicate", 3, stateSpace, prob);
+
+        Double expected = 3.;
+        Double actual = timings.getTiming(prob);
+
+        assertEquals(expected, actual, 1e-5,
+                "Measured time should be 3ns");
+    }
+
+    @Test
+    public void shouldReturnNegativeWhenOneSamplingIsUndecidable() throws Exception {
+        // Stub ProB
+        when(prob.measureEvalTime("predicate", stateSpace))
+                .thenReturn(1L)
+                .thenReturn(-1L) // undecidable
+                .thenReturn(6L);
+
+        DecisionTimings timings = new DecisionTimings("predicate", 3, stateSpace, prob);
+
+        assertTrue(0 > timings.getTiming(prob),
+                "Measured time should be negative");
+    }
+
+    @Test
+    void shouldSampleThriceForEachBackend() throws Exception {
+        // Stub ProB
+        when(prob.measureEvalTime("predicate", stateSpace))
+                .thenReturn(1L)
+                .thenReturn(2L)
+                .thenReturn(6L);
+        // Stub KodKod
+        when(kodkod.measureEvalTime("predicate", stateSpace))
+                .thenReturn(4L)
+                .thenReturn(12L)
+                .thenReturn(5L);
+
+        DecisionTimings timings = new DecisionTimings("predicate", 3, stateSpace,
+                prob, kodkod);
+
+        assertAll("Average of three runs for ProB and KodKod",
+                () -> assertEquals(3., timings.getTiming(prob), 1e-5,
+                        "Measured time for ProB should be 3ns"),
+                () -> assertEquals(7., timings.getTiming(kodkod), 1e-5,
+                        "Measured time for KodKod should be 3ns")
+        );
+    }
+
+    @Test
+    void shouldReturnArrayWithLabellingsInOrderOfBackends() throws Exception {
+        // Stub ProB
+        when(prob.measureEvalTime("predicate", stateSpace))
+                .thenReturn(1L)
+                .thenReturn(2L)
+                .thenReturn(6L);
+        // Stub KodKod
+        when(kodkod.measureEvalTime("predicate", stateSpace))
+                .thenReturn(4L)
+                .thenReturn(12L)
+                .thenReturn(5L);
+
+        DecisionTimings timings = new DecisionTimings("predicate", 3, stateSpace,
+                prob, kodkod);
+
+        assertAll("Average of three runs for ProB and KodKod",
+                () -> assertEquals(3., timings.getLabellingArray()[0], 1e-5,
+                        "Measured time for ProB should be 3ns"),
+                () -> assertEquals(7., timings.getLabellingArray()[1], 1e-5,
+                        "Measured time for KodKod should be 3ns")
+        );
+    }
+
+    @Test
+    public void shouldSampleOnceWhenUsingGenerator() throws Exception {
+        // Stub ProB
+        when(prob.measureEvalTime("predicate", stateSpace))
+                .thenReturn(1L)
+                .thenReturn(100L);
+
+        DecisionTimings timings =
+                new DecisionTimings.Generator(1, prob)
+                        .generate("predicate", stateSpace);
+
+        Double expected = 1.;
+        Double actual = timings.getTiming(prob);
+
+        assertEquals(expected, actual, 1e-5,
+                "Measured time should be 1ns");
+    }
+
+    @Test
+    public void shouldSampleThriceWhenUsingGenerator() throws Exception {
+        // Stub ProB
+        when(prob.measureEvalTime("predicate", stateSpace))
+                .thenReturn(1L)
+                .thenReturn(2L)
+                .thenReturn(6L);
+
+        DecisionTimings timings =
+                new DecisionTimings.Generator(3, prob)
+                        .generate("predicate", stateSpace);
+
+        Double expected = 3.;
+        Double actual = timings.getTiming(prob);
+
+        assertEquals(expected, actual, 1e-5,
+                "Measured time should be 3ns");
+    }
+
+    @Test
+    void shouldSampleThriceForEachBackendWhenUsingGenerator() throws Exception {
+        // Stub ProB
+        when(prob.measureEvalTime("predicate", stateSpace))
+                .thenReturn(1L)
+                .thenReturn(2L)
+                .thenReturn(6L);
+        // Stub KodKod
+        when(kodkod.measureEvalTime("predicate", stateSpace))
+                .thenReturn(4L)
+                .thenReturn(12L)
+                .thenReturn(5L);
+
+        DecisionTimings timings =
+                new DecisionTimings.Generator(3, prob, kodkod)
+                        .generate("predicate", stateSpace);
+
+        assertAll("Average of three runs for ProB and KodKod",
+                () -> assertEquals(3., timings.getTiming(prob), 1e-5,
+                        "Measured time for ProB should be 3ns"),
+                () -> assertEquals(7., timings.getTiming(kodkod), 1e-5,
+                        "Measured time for KodKod should be 3ns")
+        );
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenTimeOfOneBackendCannotBeMeasured()
+            throws Exception {
+        // Stub ProB
+        when(prob.measureEvalTime("predicate", stateSpace))
+                .thenReturn(1L)
+                .thenReturn(2L)
+                .thenReturn(6L);
+        // Stub KodKod
+        when(kodkod.measureEvalTime("predicate", stateSpace))
+                .thenReturn(4L)
+                .thenThrow(new FormulaException())
+                .thenReturn(5L);
+
+        assertThrows(LabelCreationException.class,
+                () -> new DecisionTimings("predicate", 3,
+                        stateSpace, prob, kodkod),
+                "Timings should not be creatable");
+
+    }
+}
