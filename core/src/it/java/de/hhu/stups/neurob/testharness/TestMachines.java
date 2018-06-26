76d3312748c008a5c15f6aa09d7cb5e6ee0b9e27
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2018-06-26 13:12:57 +0200

    Add list of predicates for formulae_generation.mch
    If the process of generating predicates from machine files ever changes
    even so slightly, only this file needs to be adjusted instead of
    changing every single test creating predicates from
    formulae_generation.mch.

--- core/src/it/java/de/hhu/stups/neurob/testharness/TestMachines.java
+++ core/src/it/java/de/hhu/stups/neurob/testharness/TestMachines.java
@@ -1,14 +1,32 @@
 package de.hhu.stups.neurob.testharness;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /** Unified convenience access to the machines in src/it/resources/machines */
 public class TestMachines {
+    /** Full path description of test machine directory. */
+    public static String TEST_MACHINE_DIR =
+            getMachinePath("");
+
     public static String FORMULAE_GEN_MCH =
             getMachinePath("formulae_generation.mch");
+    public static String FORMULAE_GEN_MCH_PREDICATE_FILE =
+            "formulae_generation.predicates";
+
     public static String FEATURES_CHECK_MCH =
             getMachinePath("features_check.mch");
 
+    private static Map<String, List<String>> expectedPredicates = new HashMap<>();
+
     /**
-     * Generates the path to the src/test/resources/machines/machineName file.
+     * Generates the path to the src/it/resources/machines/machineName file.
      *
      * @param machineName Name of the machine without the "machine/"
      *         direcotry prefix.
@@ -20,4 +38,34 @@ public class TestMachines {
         return TestMachines.class.getClassLoader()
                 .getResource(machineSubPath).getFile();
     }
+
+    /**
+     * Loads linewise the predicates from
+     * src/it/resources/expected_predicates/listName file.
+     * <p>
+     * The loaded predicates are cached, so the file has not to be loaded each
+     * time the list is accessed.
+     *
+     * @param listName
+     *
+     * @return
+     */
+    public static List<String> loadExpectedPredicates(String listName)
+            throws IOException {
+        if (expectedPredicates.containsKey(listName)) {
+            return expectedPredicates.get(listName);
+        }
+
+        String fullPath = TestMachines.class.getClassLoader()
+                .getResource("expected_predicates/" + listName)
+                .getFile();
+
+        Stream<String> lines = Files.lines(Paths.get(fullPath));
+        List<String> predicates = lines.collect(Collectors.toList());
+        expectedPredicates.put(listName, predicates);
+
+        return predicates;
+
+    }
+
 }
