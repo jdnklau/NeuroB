1e9e61737f9becc0dddc40baa7aa4ec29426b97c
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2018-07-16 17:00:01 +0200

    Reduce clutter in TestFeatures and -Labelling
    Instead of using own implementation of feature/labelling array, make use
    of inherited, protected fields and methods.

--- core/src/test/java/de/hhu/stups/neurob/testharness/TestFeatures.java
+++ core/src/test/java/de/hhu/stups/neurob/testharness/TestFeatures.java
@@ -3,19 +3,8 @@ package de.hhu.stups.neurob.testharness;
 import de.hhu.stups.neurob.core.features.Features;
 
 public class TestFeatures extends Features {
-    private final Double[] features;
 
     public TestFeatures(Double... features) {
-        this.features = features;
-    }
-
-    @Override
-    public int getFeatureDimension() {
-        return features.length;
-    }
-
-    @Override
-    public Double[] getFeatureArray() {
-        return features;
+        super(features);
     }
 }
