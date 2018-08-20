1294f2169fbc290c831b9482f9e02bba9718f8b2
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2018-08-20 14:09:58 +0200

    Add PredicateDump data class
    Serves as data wrapper for migrating legacy data sets into new DB
    formats.

---
+++ core/src/main/java/de/hhu/stups/neurob/training/migration/legacy/PredicateDump.java
@@ -0,0 +1,72 @@
+package de.hhu.stups.neurob.training.migration.legacy;
+
+import de.hhu.stups.neurob.core.api.backends.Backend;
+import de.hhu.stups.neurob.core.api.backends.KodKodBackend;
+import de.hhu.stups.neurob.core.api.backends.ProBBackend;
+import de.hhu.stups.neurob.core.api.backends.SmtBackend;
+import de.hhu.stups.neurob.core.api.backends.Z3Backend;
+
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Predicate dumps are a collection of predicates and their associated solving times
+ * over ProB, Kodkod, Z3, and SMT_SUPPORTED_INTERPRETER.
+ */
+public class PredicateDump {
+
+    private final String predicate;
+    private final Path source;
+    private final Map<Class<? extends Backend>, Double> timings;
+
+    public PredicateDump(String predicateDumpEntry) {
+        this(predicateDumpEntry, null);
+    }
+
+    /**
+     * @param predicateDumpEntry
+     * @param sourceMachine
+     */
+    public PredicateDump(String predicateDumpEntry, Path sourceMachine) {
+        // Split entry into labels and predicates
+        int splitPos = predicateDumpEntry.indexOf(':');
+
+        this.predicate = predicateDumpEntry.substring(splitPos + 1);
+        this.source = sourceMachine;
+
+        // Get timings
+        this.timings = new HashMap<>();
+        String[] timingLabels = predicateDumpEntry.substring(0, splitPos).split(",");
+        this.timings.put(ProBBackend.class, Double.valueOf(timingLabels[0]));
+        this.timings.put(KodKodBackend.class, Double.valueOf(timingLabels[1]));
+        this.timings.put(Z3Backend.class, Double.valueOf(timingLabels[2]));
+        this.timings.put(SmtBackend.class, Double.valueOf(timingLabels[3]));
+
+    }
+
+    public String getPredicate() {
+        return predicate;
+    }
+
+    /**
+     * Accesses the time of the given Backend from this predicate dump entry.
+     * <p>
+     * Possible values are:
+     * {@code ProBBackend.class},
+     * {@code KodkodBackend.class},
+     * {@code Z3Backend.class}, and
+     * {@code SmtBackend.class}.
+     *
+     * @param backend
+     *
+     * @return
+     */
+    public Double getTime(Class<? extends Backend> backend) {
+        return timings.get(backend);
+    }
+
+    public Path getSource() {
+        return source;
+    }
+}
---
+++ core/src/test/java/de/hhu/stups/neurob/training/migration/legacy/PredicateDumpTest.java
@@ -0,0 +1,72 @@
+package de.hhu.stups.neurob.training.migration.legacy;
+
+import de.hhu.stups.neurob.core.api.backends.KodKodBackend;
+import de.hhu.stups.neurob.core.api.backends.ProBBackend;
+import de.hhu.stups.neurob.core.api.backends.SmtBackend;
+import de.hhu.stups.neurob.core.api.backends.Z3Backend;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PredicateDumpTest {
+
+    @Test
+    public void shouldSeparatePredicateFromEntry() {
+        String entry = "1.23456789E8,2.23456789E8,3.23456789E8,-1.0:x>0";
+        PredicateDump pdump = new PredicateDump(entry);
+
+        String expected = "x>0";
+        String actual = pdump.getPredicate();
+
+        assertEquals(expected, actual,
+                "Extracted Predicate does not match");
+    }
+
+    @Test
+    public void shouldKeepPredicateAsOneWhenItContainsColonsItself() {
+        String entry = "1.23456789E8,2.23456789E8,3.23456789E8,-1.0:x:NAT";
+        PredicateDump pdump = new PredicateDump(entry);
+
+        String expected = "x:NAT";
+        String actual = pdump.getPredicate();
+
+        assertEquals(expected, actual,
+                "Extracted Predicate does not match");
+    }
+
+    @Test
+    public void shouldExtractLabellingTimes() {
+        String entry = "1.23456789E8,2.23456789E8,3.23456789E8,-1.0:x:NAT";
+        PredicateDump pdump = new PredicateDump(entry);
+
+        assertAll("Checking timing extraction",
+                () -> assertEquals(1.23456789E8,
+                        pdump.getTime(ProBBackend.class),
+                        1e-10,
+                        "ProB timing is off"),
+                () -> assertEquals(2.23456789E8,
+                        pdump.getTime(KodKodBackend.class),
+                        1e-10,
+                        "Kodkod timing is off"),
+                () -> assertEquals(3.23456789E8,
+                        pdump.getTime(Z3Backend.class),
+                        1e-10,
+                        "Z3 timing is off"),
+                () -> assertEquals(-1.0,
+                        pdump.getTime(SmtBackend.class),
+                        1e-10,
+                        "SMT_SUPPORTED_INTERPRETER timing is off")
+        );
+    }
+
+    @Test
+    public void shouldHaveNullSourceWhenNoSourceIsGiven() {
+        String entry = "1.23456789E8,2.23456789E8,3.23456789E8,-1.0:x:NAT";
+        PredicateDump pdump = new PredicateDump(entry);
+
+        assertNull(pdump.getSource(),
+                "No source should be given");
+    }
+}
