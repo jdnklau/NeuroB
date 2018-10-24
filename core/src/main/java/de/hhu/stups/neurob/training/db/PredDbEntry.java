5dc10bd2985fe2e9173241fee50dfdcab95521ae
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2018-10-24 13:33:17 +0200

    Add generator and array utility to PredDbEntry

--- core/src/main/java/de/hhu/stups/neurob/training/db/PredDbEntry.java
+++ core/src/main/java/de/hhu/stups/neurob/training/db/PredDbEntry.java
@@ -1,5 +1,6 @@
 package de.hhu.stups.neurob.training.db;
 
+import de.hhu.stups.neurob.core.api.backends.Answer;
 import de.hhu.stups.neurob.core.api.backends.Backend;
 import de.hhu.stups.neurob.core.api.backends.KodkodBackend;
 import de.hhu.stups.neurob.core.api.backends.ProBBackend;
@@ -8,24 +9,41 @@ import de.hhu.stups.neurob.core.api.backends.TimedAnswer;
 import de.hhu.stups.neurob.core.api.backends.Z3Backend;
 import de.hhu.stups.neurob.core.api.bmethod.BMachine;
 import de.hhu.stups.neurob.core.api.bmethod.BPredicate;
+import de.hhu.stups.neurob.core.api.bmethod.MachineAccess;
+import de.hhu.stups.neurob.core.exceptions.FormulaException;
+import de.hhu.stups.neurob.core.exceptions.LabelCreationException;
+import de.hhu.stups.neurob.core.exceptions.MachineAccessException;
+import de.hhu.stups.neurob.core.labelling.PredicateLabelGenerating;
 import de.hhu.stups.neurob.core.labelling.PredicateLabelling;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.nio.file.Path;
+import java.time.Period;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class PredDbEntry extends PredicateLabelling {
 
     private final BPredicate pred;
     private final BMachine source;
     private final Map<Backend, TimedAnswer> results;
+    public static final Long DEFAULT_TIMEOUT = 2500L;
+    public static final TimeUnit DEFAULT_TIMEUNIT = TimeUnit.MILLISECONDS;
+
+    private static final Logger log =
+            LoggerFactory.getLogger(PredDbEntry.class);
 
     /**
      * Array over backends in use, also providing an ordering by id.
-     * 0 - ProB
-     * 1 - Kodkod
-     * 2 - Z3
-     * 3 - SMT_SUPPORTED_INTERPRETER
+     * <ol start="0">
+     * <li>ProB</li>
+     * <li>Kodkod</li>
+     * <li>Z3</li>
+     * <li>SMT_SUPPORTED_INTERPRETER</li>
+     * </ol>
      */
     public final static Backend[] BACKENDS_USED = {
             new ProBBackend(),
@@ -111,6 +129,30 @@ public class PredDbEntry extends PredicateLabelling {
         return map;
     }
 
+    /**
+     * Collects and returns the backends' answers as array,
+     * ordered by {@link #BACKENDS_USED}.
+     *
+     * @return
+     */
+    public TimedAnswer[] getAnswerArray() {
+        return getAnswerArray(BACKENDS_USED);
+    }
+
+    /**
+     * Collects and returns the backend's answers as array in corresponding order.
+     *
+     * @param backends
+     *
+     * @return
+     */
+    public TimedAnswer[] getAnswerArray(Backend... backends) {
+        return Arrays.stream(backends)
+                .map(results::get)
+                .toArray(TimedAnswer[]::new);
+
+    }
+
     public BPredicate getPredicate() {
         return pred;
     }
@@ -121,5 +163,107 @@ public class PredDbEntry extends PredicateLabelling {
 
     public Map<Backend, TimedAnswer> getResults() {
         return results;
+    }
+
+    public static class Generator implements PredicateLabelGenerating<PredDbEntry> {
+
+        private int samplingSize;
+        private final Long timeout;
+        private final TimeUnit timeUnit;
+        private final Backend[] backends;
+
+        /**
+         * @param samplingSize Number of measurements per backend,
+         *         from which the average run time is taken.
+         * @param backends Backends to run in given order for each predicate
+         * @param timeout
+         * @param timeUnit
+         */
+        public Generator(int samplingSize, Backend[] backends, Long timeout, TimeUnit timeUnit) {
+            this.samplingSize = samplingSize;
+            this.timeout = timeout;
+            this.timeUnit = timeUnit;
+            this.backends = backends;
+        }
+
+        /**
+         * @param samplingSize Number of measurements per backend,
+         *         from which the average run time is taken.
+         * @param backends
+         */
+        public Generator(int samplingSize, Backend[] backends) {
+            this(samplingSize, backends, PredDbEntry.DEFAULT_TIMEOUT, PredDbEntry.DEFAULT_TIMEUNIT);
+        }
+
+
+        @Override
+        public PredDbEntry generate(BPredicate predicate, BMachine bMachine) throws LabelCreationException {
+            // Get machine access
+            MachineAccess machineAccess;
+            try {
+                machineAccess = (bMachine != null)
+                        ? bMachine.getMachineAccess()
+                        : null;
+            } catch (MachineAccessException e) {
+                throw new LabelCreationException(
+                        "Could not sample predicate due to machine access problems", e);
+            }
+
+            // Gather results
+            Map<Backend, TimedAnswer> results = new HashMap<>();
+            for (Backend b : backends) {
+                TimedAnswer answer;
+                try {
+                    answer = samplePredicate(predicate, b, machineAccess);
+                } catch (LabelCreationException e) {
+                    log.error("Unable to sample {} with backend {}",
+                            predicate, b, e);
+                    answer = null;
+                }
+
+                results.put(b, answer);
+            }
+
+            if (machineAccess != null) {
+                bMachine.closeMachineAccess();
+            }
+
+            return new PredDbEntry(predicate, bMachine, results);
+        }
+
+        public TimedAnswer samplePredicate(BPredicate pred, Backend backend, MachineAccess bMachine)
+                throws LabelCreationException {
+            log.trace("Sampling timings over backend {}; {} times",
+                    backend, samplingSize);
+
+            if (samplingSize == 0) {
+                return null;
+            }
+
+            Long sampled = 0L; // sum of all run times
+            TimedAnswer lastAnswer = null;
+            for (int i = 0; i < samplingSize; i++) {
+                try {
+                    TimedAnswer timing = backend.solvePredicate(pred, bMachine,
+                            timeout, timeUnit);
+                    // stop if already error or timeout
+                    Answer answer = timing.getAnswer();
+                    if (answer.equals(Answer.ERROR) || answer.equals(Answer.TIMEOUT)) {
+                        return timing;
+                    }
+                    sampled += timing.getTime();
+                    lastAnswer = timing;
+                } catch (FormulaException e) {
+                    throw new LabelCreationException(
+                            "Could not create timing sample #" + i
+                            + " for " + backend.toString() + "over predicate "
+                            + pred.toString(),
+                            e);
+                }
+            }
+
+            return new TimedAnswer(
+                    lastAnswer.getAnswer(), sampled / samplingSize, lastAnswer.getMessage());
+        }
     }
 }
--- core/src/test/java/de/hhu/stups/neurob/training/db/PredDbEntryTest.java
+++ core/src/test/java/de/hhu/stups/neurob/training/db/PredDbEntryTest.java
@@ -7,14 +7,19 @@ import de.hhu.stups.neurob.core.api.backends.ProBBackend;
 import de.hhu.stups.neurob.core.api.backends.SmtBackend;
 import de.hhu.stups.neurob.core.api.backends.TimedAnswer;
 import de.hhu.stups.neurob.core.api.backends.Z3Backend;
+import de.hhu.stups.neurob.core.api.bmethod.BMachine;
 import de.hhu.stups.neurob.core.api.bmethod.BPredicate;
-import org.junit.jupiter.api.Disabled;
+import de.hhu.stups.neurob.core.exceptions.FormulaException;
+import de.hhu.stups.neurob.core.exceptions.LabelCreationException;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class PredDbEntryTest {
 
@@ -87,7 +92,7 @@ class PredDbEntryTest {
                 new TimedAnswer(Answer.ERROR, 400L)
         };
 
-        Map<Backend, TimedAnswer> expected =new HashMap<>();
+        Map<Backend, TimedAnswer> expected = new HashMap<>();
         expected.put(backendOrder[0], new TimedAnswer(Answer.VALID, 100L));
         expected.put(backendOrder[1], new TimedAnswer(Answer.INVALID, 200L));
         expected.put(backendOrder[2], new TimedAnswer(Answer.UNKNOWN, 300L));
@@ -123,6 +128,131 @@ class PredDbEntryTest {
 
         assertArrayEquals(expected, actual);
 
+    }
+
+    @Test
+    void shouldAnswerWithAverageTimeOfAllRuns() throws FormulaException, LabelCreationException {
+        Backend backend = mock(Backend.class);
+        when(backend.solvePredicate(any(), any(), any(), any()))
+                .thenReturn(new TimedAnswer(Answer.VALID, 10L))
+                .thenReturn(new TimedAnswer(Answer.VALID, 20L))
+                .thenReturn(new TimedAnswer(Answer.VALID, 30L))
+                .thenReturn(new TimedAnswer(Answer.VALID, 80L));
+
+        PredDbEntry.Generator generator =
+                new PredDbEntry.Generator(4, null, null, null);
+
+        TimedAnswer expected = new TimedAnswer(Answer.VALID, 35L);
+        TimedAnswer actual = generator.samplePredicate(null, backend, null);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldGenerateSamplesForEachBackend() throws FormulaException, LabelCreationException {
+        // Set up two backends to call
+        Backend back1 = mock(Backend.class);
+        when(back1.solvePredicate(any(), any(), any(), any()))
+                .thenReturn(new TimedAnswer(Answer.VALID, 10L))
+                .thenReturn(new TimedAnswer(Answer.VALID, 20L));
+        Backend back2 = mock(Backend.class);
+        when(back2.solvePredicate(any(), any(), any(), any()))
+                .thenReturn(new TimedAnswer(Answer.INVALID, 30L))
+                .thenReturn(new TimedAnswer(Answer.INVALID, 40L));
+        Backend[] backends = {back1, back2};
+
+        PredDbEntry.Generator generator =
+                new PredDbEntry.Generator(2, backends, null, null);
+
+        PredDbEntry expected = new PredDbEntry(BPredicate.of("pred"),
+                null, backends,
+                new TimedAnswer(Answer.VALID, 15L),
+                new TimedAnswer(Answer.INVALID, 35L));
+        PredDbEntry actual = generator.generate(BPredicate.of("pred"),
+                null);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldGenerateNullSamplesWhenBackendThrowsException() throws FormulaException, LabelCreationException {
+        // Set up two backends to call
+        Backend back1 = mock(Backend.class);
+        when(back1.solvePredicate(any(), any(), any(), any()))
+                .thenReturn(new TimedAnswer(Answer.VALID, 10L))
+                .thenReturn(new TimedAnswer(Answer.VALID, 20L));
+        Backend back2 = mock(Backend.class);
+        when(back2.solvePredicate(any(), any(), any(), any()))
+                .thenThrow(new FormulaException("This exception is hard coded for a unit test"));
+        Backend[] backends = {back1, back2};
+
+        PredDbEntry.Generator generator =
+                new PredDbEntry.Generator(2, backends, null, null);
+
+        PredDbEntry expected = new PredDbEntry(BPredicate.of("pred"),
+                null, backends,
+                new TimedAnswer(Answer.VALID, 15L),
+                null);
+        PredDbEntry actual = generator.generate(BPredicate.of("pred"),
+                null);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCollectResultMapToArray() {
+        Backend[] backendOrder = {new ProBBackend(), new KodkodBackend(), new Z3Backend()};
+
+        Map<Backend, TimedAnswer> resultMap = new HashMap<>();
+        resultMap.put(backendOrder[0], new TimedAnswer(Answer.VALID, 100L));
+        resultMap.put(backendOrder[1], new TimedAnswer(Answer.INVALID, 200L));
+        resultMap.put(backendOrder[2], new TimedAnswer(Answer.UNKNOWN, 300L));
+
+        TimedAnswer[] expected = {
+                new TimedAnswer(Answer.VALID, 100L),
+                new TimedAnswer(Answer.INVALID, 200L),
+                new TimedAnswer(Answer.UNKNOWN, 300L)
+        };
+        TimedAnswer[] actual = new PredDbEntry(null, null, resultMap).getAnswerArray(backendOrder);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCollectResultMapToArrayWithNullEntriesIfSomeDataIsMissing() {
+        Backend[] backendOrder = {new ProBBackend(), new KodkodBackend(), new Z3Backend()};
+
+        Map<Backend, TimedAnswer> resultMap = new HashMap<>();
+        resultMap.put(backendOrder[0], new TimedAnswer(Answer.VALID, 100L));
+        resultMap.put(backendOrder[2], new TimedAnswer(Answer.UNKNOWN, 300L));
+
+        TimedAnswer[] expected = {
+                new TimedAnswer(Answer.VALID, 100L),
+                null,
+                new TimedAnswer(Answer.UNKNOWN, 300L)
+        };
+        TimedAnswer[] actual = new PredDbEntry(null, null, resultMap).getAnswerArray(backendOrder);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCollectSubsetOfDataWhenSubsetOfBackendsIsSpecified() {
+        Backend[] backendOrder = {new ProBBackend(), new KodkodBackend(), new Z3Backend()};
+
+        Map<Backend, TimedAnswer> resultMap = new HashMap<>();
+        resultMap.put(backendOrder[0], new TimedAnswer(Answer.VALID, 100L));
+        resultMap.put(backendOrder[1], new TimedAnswer(Answer.INVALID, 200L));
+        resultMap.put(backendOrder[2], new TimedAnswer(Answer.UNKNOWN, 300L));
+
+        TimedAnswer[] expected = {
+                new TimedAnswer(Answer.VALID, 100L),
+                new TimedAnswer(Answer.UNKNOWN, 300L)
+        };
+        TimedAnswer[] actual = new PredDbEntry(null, null, resultMap)
+                .getAnswerArray(new ProBBackend(), new Z3Backend());
+
+        assertArrayEquals(expected, actual);
     }
 
 
