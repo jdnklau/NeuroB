73f71b2e20c937c88f429bb1b2a8f8b150f5e33f
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2019-01-09 10:11:22 +0100

    Add sampling functionality to LabelGenerating interface

--- core/src/main/java/de/hhu/stups/neurob/core/labelling/LabelGenerating.java
+++ core/src/main/java/de/hhu/stups/neurob/core/labelling/LabelGenerating.java
@@ -1,6 +1,11 @@
 package de.hhu.stups.neurob.core.labelling;
 
 import de.hhu.stups.neurob.core.exceptions.LabelCreationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Functional interface for a labelling step.
@@ -14,5 +19,29 @@ import de.hhu.stups.neurob.core.exceptions.LabelCreationException;
  */
 @FunctionalInterface
 public interface LabelGenerating<L extends Labelling, In> {
+
     L generate(In source) throws LabelCreationException;
+
+    /**
+     * Calls {@link #generate(Object)} consecutively for the specified amount
+     * of times, returning the labels in a list.
+     *
+     * This is meant for cases in which the labelling might slightly differ
+     * from call to call, allowing to conveniently gather multiple values,
+     * e.g. in a regression over runtime.
+     *
+     * @param source
+     * @param sampleSize
+     * @return
+     */
+    default List<L> generateSamples(In source, int sampleSize) throws LabelCreationException {
+        List<L> results = new ArrayList<>();
+
+        for (int i = 0; i< sampleSize; i++) {
+            // Logging?
+            results.add(generate(source));
+        }
+
+        return results;
+    }
 }
--- core/src/main/java/de/hhu/stups/neurob/core/labelling/PredicateLabelGenerating.java
+++ core/src/main/java/de/hhu/stups/neurob/core/labelling/PredicateLabelGenerating.java
@@ -6,6 +6,9 @@ import de.hhu.stups.neurob.core.api.bmethod.MachineAccess;
 import de.hhu.stups.neurob.core.exceptions.LabelCreationException;
 import de.hhu.stups.neurob.core.exceptions.MachineAccessException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Generates a labelling for a given predicate.
  *
@@ -33,13 +36,81 @@ public interface PredicateLabelGenerating<L extends PredicateLabelling>
         try {
             MachineAccess access = (bMachine != null) ? bMachine.spawnMachineAccess() : null;
             labelling = generate(predicate, access);
-            if(access != null) {
+            if (access != null) {
                 access.close();
             }
         } catch (MachineAccessException e) {
             throw new LabelCreationException("Could not create features for " + predicate, e);
         }
         return labelling;
+    }
+
+    /**
+     * Calls {@link #generate(BPredicate, MachineAccess)})} consecutively for the specified amount
+     * of times, returning the labels in a list.
+     * <p>
+     * This is meant for cases in which the labelling might slightly differ
+     * from call to call, allowing to conveniently gather multiple values,
+     * e.g. in a regression over runtime.
+     *
+     * @param predicate
+     *
+     * @return
+     */
+    default List<L> generateSamples(BPredicate predicate, int sampleSize) throws LabelCreationException {
+        return generateSamples(predicate, (MachineAccess) null, sampleSize);
+    }
+
+    /**
+     * Calls {@link #generate(BPredicate, MachineAccess)})} consecutively for the specified amount
+     * of times, returning the labels in a list.
+     * <p>
+     * This is meant for cases in which the labelling might slightly differ
+     * from call to call, allowing to conveniently gather multiple values,
+     * e.g. in a regression over runtime.
+     *
+     * @param predicate
+     * @param bMachine
+     *
+     * @return
+     */
+    default List<L> generateSamples(BPredicate predicate, BMachine bMachine, int sampleSize)
+            throws LabelCreationException {
+        List<L> labelling;
+        try {
+            MachineAccess access = (bMachine != null) ? bMachine.spawnMachineAccess() : null;
+            labelling = generateSamples(predicate, access, sampleSize);
+            if (access != null) {
+                access.close();
+            }
+        } catch (MachineAccessException e) {
+            throw new LabelCreationException("Could not create features for " + predicate, e);
+        }
+        return labelling;
+    }
+
+    /**
+     * Calls {@link #generate(BPredicate, MachineAccess)})} consecutively for the specified amount
+     * of times, returning the labels in a list.
+     * <p>
+     * This is meant for cases in which the labelling might slightly differ
+     * from call to call, allowing to conveniently gather multiple values,
+     * e.g. in a regression over runtime.
+     *
+     * @param predicate
+     * @param access
+     *
+     * @return
+     */
+    default List<L> generateSamples(BPredicate predicate, MachineAccess access, int sampleSize)
+            throws LabelCreationException {
+        List<L> results = new ArrayList<>();
+
+        for (int i = 0; i < sampleSize; i++) {
+            results.add(generate(predicate, access));
+        }
+
+        return results;
     }
 
     /**
