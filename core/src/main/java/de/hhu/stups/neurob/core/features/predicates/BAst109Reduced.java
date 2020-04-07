ca3af099d851ec8d15fc27c2f417d793eb6561a0
Jannik Dunkelau <jannik.dunkelau@hhu.de>
2020-04-07 12:34:10 +0200

    Fix in logicOp count logic for F109

--- core/src/main/java/de/hhu/stups/neurob/core/features/predicates/BAst109Reduced.java
+++ core/src/main/java/de/hhu/stups/neurob/core/features/predicates/BAst109Reduced.java
@@ -114,7 +114,7 @@ public class BAst109Reduced extends PredicateFeatures {
                     + data.getTailRestrictionCount()
                     + data.getGeneralConcatCount();
             double ids = data.getIdentifiersCount();
-            double logicOps = ((data.getConjunctionsCount() - conjuncts)
+            double logicOps = ((data.getConjunctionsCount() - conjuncts + 1)
                                + data.getDisjunctionsCount()
                                + data.getImplicationsCount()
                                + data.getEquivalencesCount());
